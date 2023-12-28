226748103a1caa23a9544103410b91426f473f59
br8832 <70492495+br8832@users.noreply.github.com>
2023-12-28 15:52:15 -0800

    time to hash

--- React/src/Components/Test.js
+++ React/src/Components/Test.js
@@ -15,13 +15,13 @@ export default class Test extends React.Component{
     render(){
         return (
         <>
-        <button onClick={()=>{this.props.getChildData(this.state.value);this.setState({show:!this.state.show})}}>Pass To Parent</button>
-        <input onChange={(_)=>this.setState({value:e.target.value})} style={{display:'block',width:'50%'}} type='text' placeholder="Pass the text for child compnent to pass"></input>
-        <div class="box test" style={{display: this.state.show ? 'flex' : 'none' }}>  
-        <div class="box-item test">
+        <button onClick={()=>{this.props.getChildData(this.state.value);this.setState({show:!this.state.show});let val=setInterval(()=>console.log('Set Timeout Words'),1000);setTimeout(()=>clearInterval(val),2000)}}>Pass To Parent</button>
+        <input onChange={(e)=>this.setState({value:e.target.value})} style={{display:'block',width:'50%'}} type='text' placeholder="Pass the text for child compnent to pass"></input>
+        <div className="box test" style={{display: this.state.show ? 'flex' : 'none' }}>  
+        <div className="box-item test">
             <h2>Shopping Cart Application from {this.props.parent}</h2>
         </div>
-        <div class="box-item test">
+        <div className="box-item test">
             <h2>{this.props.username}</h2>
             <p>es un tio de putisima madre</p>
         </div>
